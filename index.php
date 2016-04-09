173c9707d69c73ff3cf876192c7bae126f4354fa
Masoud A <amini141@gmail.com>
2016-04-09 10:43:17 +0430

    update

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: درگاه زرین گیت گراویتی فرم
+Plugin URI: http://gravityforms.ir/
+Description: افزونه درگاه پرداخت زرین گیت برای فرم ساز فوق پیشرفته Gravity Forms
+Version: 2.0.0
+Author: حنان ابراهیمی ستوده
+Author URI: http://webforest.ir/
+*/
+if (!defined( 'ABSPATH' )) exit;
+require_once('zaringate.php');
